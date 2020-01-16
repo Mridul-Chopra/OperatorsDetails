e17dfadd5cd6f211bd0d75ae7ff99f63c1f688a2
Mridul Chopra <mridul.chopra@paxcel.net>
2020-01-16 19:30:13 +0530

    first commit

---
+++ src/net/Paxcel/Driver.java
@@ -0,0 +1,18 @@
+package net.Paxcel;
+
+public class Driver 
+{
+
+	public static void main(String[] args)	
+	{
+		try 
+		{
+			
+		}
+		catch(Exception e)
+		{
+			System.out.println("Some error occoured");
+		}
+
+	}		
+}
---
+++ src/net/Paxcel/Resources.java
@@ -0,0 +1,61 @@
+package net.Paxcel;
+
+import java.io.FileInputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.*;
+import org.apache.log4j.*;
+
+public class Resources {
+	
+	public static Logger log;
+	public static Properties connectionProp;
+	public static Connection conn;
+	
+	
+	static
+	{
+		try
+		{
+		log = Logger.getLogger("GLOBAL");
+		Properties prop = new Properties();
+		prop.load(new FileInputStream("src\\resources\\log4j.properties"));
+		PropertyConfigurator.configure(prop);
+		log.setLevel(Level.ERROR);
+		}
+		catch(Exception e)
+		{
+			System.out.println("Cannot load resources. Exiting");
+			System.exit(0);
+		}
+	}
+	
+	
+	static
+	{
+		try
+		{
+			connectionProp = new Properties();
+			connectionProp.load(new FileInputStream("src\\resources\\Connection.properties"));
+			PropertyConfigurator.configure(connectionProp);
+			
+			Class.forName(connectionProp.getProperty("DB.Driver"));
+			conn = DriverManager.getConnection(connectionProp.getProperty("DB.url"));
+			
+			String url = connectionProp.getProperty("DB.url");
+			String username = connectionProp.getProperty("DB.username");
+			String password = connectionProp.getProperty("DB.password");
+			
+			conn = DriverManager.getConnection(url,username,password);
+			
+		}
+		catch(Exception e)
+		{
+			System.out.println("Fatal Error. Exiting");
+			System.exit(0);
+		}
+		
+	}
+	
+
+}
